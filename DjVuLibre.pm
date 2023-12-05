8535b7456589856c0a45d7061a823ad8f8192d59
Michal Josef Špaček <tupinek@gmail.com>
2023-12-05 14:12:52 +0100

    Prepare for 0.08 version

--- DjVuLibre.pm
+++ DjVuLibre.pm
@@ -4,7 +4,7 @@ use parent qw(Alien::Base);
 use strict;
 use warnings;
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 1;
 
@@ -96,6 +96,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
